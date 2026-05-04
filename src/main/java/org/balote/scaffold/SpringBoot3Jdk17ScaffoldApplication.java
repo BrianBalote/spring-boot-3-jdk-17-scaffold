1fa6434ec8279a7415775296c421c681295bcd29
Brian Balote <bbalote@lendingpoint.com>
2026-05-04 20:00:20 +0800

    feat(main): commit spring initialized files

---
+++ src/main/java/org/balote/scaffold/SpringBoot3Jdk17ScaffoldApplication.java
@@ -0,0 +1,13 @@
+package org.balote.scaffold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBoot3Jdk17ScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBoot3Jdk17ScaffoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/balote/scaffold/SpringBoot3Jdk17ScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.balote.scaffold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot3Jdk17ScaffoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
