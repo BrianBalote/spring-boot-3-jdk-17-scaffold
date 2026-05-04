b751afe528cd29ae27ab64a6ff73519b7a4777c8
Brian Balote <bbalote@lendingpoint.com>
2026-05-04 21:54:26 +0800

    feat(main): use datastax driver instead of cassandra, add sybase

---
+++ src/main/java/org/balote/scaffold/config/CassandraConfig.java
@@ -0,0 +1,9 @@
+package org.balote.scaffold.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class CassandraConfig {
+}
